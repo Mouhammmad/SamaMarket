95c0575d4ba7f5fa7f4be990f10fd7594b32c04c
issaga <isshagadiallo77@gmail.com>
2026-07-28 21:38:44 +0000

    Configuration du Backend pour que Angular consomme les API

--- Categorie/models.py
+++ Categorie/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Categorie(models.Model):
     nom=models.CharField(max_length=100)
+    count = models.IntegerField( default=0)
     description = models.TextField(blank=True)
     
 
