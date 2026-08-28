9c51ea91703845c1fa30c452085d4c9f3b44f2cd
Moustapha BALDE <moustaphabaldehotmail@gmail.com>
2026-08-28 20:39:38 +0000

    modified:   Marketplace/Marketplace/settings.py

--- Marketplace/Marketplace/settings.py
+++ Marketplace/Marketplace/settings.py
@@ -109,12 +109,21 @@ WSGI_APPLICATION = 'Marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-    )
-}
+database_url = os.getenv('DATABASE_URL', '').strip()
+if database_url:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            database_url,
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
